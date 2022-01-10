da55513018c699690d2fff14f8bd2ea8c29f6cc0
Daan Wissing <daan.wissing@gmail.com>
2022-01-10 20:13:13 +0100

    Show first!

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using TG.Blazor.IndexedDB;
+using thanks.Services;
 
 namespace thanks
 {
@@ -19,6 +20,7 @@ namespace thanks
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IThankService, ThankService>();
 
             builder.Services.AddIndexedDB(dbStore => {
                 dbStore.DbName = "ThanksDb";
--- Services/ThankService.cs
+++ Services/ThankService.cs
@@ -2,41 +2,45 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using TG.Blazor.IndexedDB;
 
-public class ThankService : IThankService
+namespace thanks.Services
 {
-    private readonly IndexedDBManager dbManager;
 
-    public ThankService(IndexedDBManager dbManager)
+    public class ThankService : IThankService
     {
-        this.dbManager = dbManager;
-    }
+        private readonly IndexedDBManager dbManager;
 
-    public Task<IEnumerable<ThanksNote>> GetPreviousThanks(int amount)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Task<ThanksNote> GetRandomThanks()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public async Task SaveThanks(ThanksNote note)
-    {
-        var record = new StoreRecord<ThanksNote>
+        public ThankService(IndexedDBManager dbManager)
         {
-            Storename = "ThanksNotes",
-            Data = note,
-        };
-        await dbManager.AddRecord(record);
+            this.dbManager = dbManager;
+        }
+
+        public Task<IEnumerable<ThanksNote>> GetPreviousThanks(int amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ThanksNote> GetRandomThanks()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task SaveThanks(ThanksNote note)
+        {
+            var record = new StoreRecord<ThanksNote>
+            {
+                Storename = "ThanksNotes",
+                Data = note,
+            };
+            await dbManager.AddRecord(record);
+        }
     }
-}
 
-public interface IThankService 
-{
-    Task SaveThanks(ThanksNote note);
+    public interface IThankService
+    {
+        Task SaveThanks(ThanksNote note);
 
-    Task<ThanksNote> GetRandomThanks();
+        Task<ThanksNote> GetRandomThanks();
 
-    Task<IEnumerable<ThanksNote>> GetPreviousThanks(int amount);
+        Task<IEnumerable<ThanksNote>> GetPreviousThanks(int amount);
+    }
 }
